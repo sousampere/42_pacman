d7a1945bf78f074d656a4835a403d0de7795dc97
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2026-05-02 23:49:12 +0200

    fix: added description in fields && custom error msg && support for level key in config

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -4,40 +4,66 @@ from pydantic import BaseModel, Field, ValidationError, model_validator
 
 
 class ConfigError(Exception):
-    pass
+    def __init__(self, msg: str = "") -> None:
+        super().__init__(f"Config Error: {msg}")
 
 
 class ConfigFileError(ConfigError):
-    pass
+    def __init__(self, msg: str = "") -> None:
+        super().__init__(f"Config File Error: {msg}")
 
 
 class ConfigJSONError(ConfigError):
-    pass
+    def __init__(self, msg: str = "") -> None:
+        super().__init__(f"Config Json Error: {msg}")
 
 
 class Config(BaseModel):
-    highscore_filename: str = Field(default="leaderboard.json")
-    lvl_width: int = Field(ge=1, le=100, default=16)
-    lvl_height: int = Field(ge=1, le=100, default=10)
-    lives: int = Field(ge=1, default=20)
-    pacgum_points: int = Field(ge=0, default=10)
-    super_pacgum_points: int = Field(ge=0, default=50)
-    ghost_points: int = Field(ge=0, default=200)
-    seed: str = Field(default="forty-two")
-    max_time: int = Field(ge=1, default=90)
-    signature: str = Field(default="")
+    highscore_filename: str = Field(
+        default="leaderboard.json", description="Leaderboard input/output source"
+    )
+    level: list[dict[str, int]] = Field(
+        default=[{"width": 20, "height": 20}] * 10,
+        description="List of levels dimensions (width,height)",
+    )
+    lives: int = Field(ge=1, default=20, description="Number of lives of the player")
+    pacgum_points: int = Field(
+        ge=0, default=10, description="Points earned for each pacgum eaten"
+    )
+    super_pacgum_points: int = Field(
+        ge=0, default=50, description="Points earned for each super-pacgum"
+    )
+    ghost_points: int = Field(
+        ge=0, default=200, description="Points earned for each ghost eatten"
+    )
+    seed: str = Field(
+        default="forty-two", description="Seed on which the random generation is based"
+    )
+    max_time: int = Field(ge=1, default=90, description="Max duration of each level")
+    signature: str = Field(
+        default="", description="Signature of the config (automatically generated)"
+    )
 
     @model_validator(mode="after")
     def validate_data(self) -> "Config":
         """Validate the data more precisely after setting variables"""
         import hashlib
 
+        # Verify there is enough levels
+        if len(self.level) < 10:
+            raise ConfigError("Not enough levels in config(must be >= 10)")
+
         # Create signature
         signature_str = (
-            f"{self.lvl_width}{self.lvl_height}"
             f"{self.lives}{self.pacgum_points}{self.super_pacgum_points}"
             f"{self.ghost_points}{self.seed}{self.max_time}"
         )
+        for i in range(len(self.level)):
+            signature_str = (
+                signature_str
+                + str(self.level[i]["width"])
+                + str(self.level[i]["height"])
+            )
         hash_obj = hashlib.sha256(signature_str.encode("utf-8"))
         self.signature = hash_obj.hexdigest()
 
@@ -57,6 +83,20 @@ class ABCConfigLoader(ABC):
 
 class ConfigLoader(ABCConfigLoader):
     """Interface of ABCConfigLoader for loading a local json file"""
+
+    @staticmethod
+    def warn_missing_key(key: str) -> None:
+        print(
+            f"[Warning] No value set for {key} in your config. "
+            "Continuing with default values."
+        )
+
+    @staticmethod
+    def warn_invalid_key(key: str) -> None:
+        print(
+            f"[Warning] Invalid value for {key} in your config. "
+            "Continuing with default values."
+        )
 
     @staticmethod
     def remove_comments(input_str: str) -> str:
@@ -95,13 +135,78 @@ class ConfigLoader(ABCConfigLoader):
             raise ConfigFileError("Invalid JSON format")
 
         # Print warning message for unset variables
-        for item in ["highscore_filename", "lvl_width",
-                     "lvl_height", "lives", "pacgum_points",
-                     "super_pacgum_points", "ghost_points",
-                     "seed", "max_time"]:
-            if isinstance(data, dict) and item not in data.keys():
-                print(f"[Warning] No value set for {item} in your config."
-                      f"Continuing with default values.")
+        for param in [
+            "highscore_filename",
+            "level",
+            "lives",
+            "pacgum_points",
+            "super_pacgum_points",
+            "ghost_points",
+            "seed",
+            "max_time",
+        ]:
+            if isinstance(data, dict) and param not in data.keys():
+                print(
+                    f"[Warning] No value set for {param} in your config."
+                    f"Continuing with default values."
+                )
+
+        # Verify every data type. Remove it if invalid
+        # int
+        for param in [
+            "lives",
+            "pacgum_points",
+            "super_pacgum_points",
+            "ghost_points",
+            "max_time",
+        ]:
+            if param in data.keys() and type(data[param]) is not int:
+                data.pop(param)  # remove item if not valid
+                ConfigLoader.warn_invalid_key(param)
+        # string
+        for param in ["highscore_filename", "seed"]:
+            if param in data.keys() and type(data[param]) is not str:
+                data.pop(param)  # remove item if not valid
+                ConfigLoader.warn_invalid_key(param)
+
+        # Verify 'level' key validity. Remove it if invalid
+        if "level" in data.keys():
+            if type(data["level"]) is not list:
+                ConfigLoader.warn_missing_key("level")
+                data.pop("level")
+        if "level" in data.keys():
+            for lvl in data["level"]:
+                if (
+                    type(lvl) is not dict
+                    or "width" not in lvl.keys()
+                    or "height" not in lvl.keys()
+                ):
+                    print(
+                        "[Warning] Removed an invalid "
+                        "level in your config."
+                    )
+                    ConfigLoader.warn_invalid_key("level")
+                    data.pop("level")
+                    data['level'].remove(lvl)
+                else:
+                    if (
+                        type(lvl["width"]) is not int
+                        or lvl["width"] < 1
+                        or lvl["width"] > 50
+                        or type(lvl["height"]) is not int
+                        or lvl["height"] < 1
+                        or lvl["height"] > 50
+                    ):
+                        print(
+                            "[Warning] Removed an invalid "
+                            "level in your config."
+                        )
+                        data['level'].remove(lvl)
+        if "level" in data.keys():
+            if len(data["level"]) < 10:
+                print("[Warning] Not enough levels in config. "
+                      "Using default levels.")
+                data.pop("level")
 
         # Create the Config object
         try:
